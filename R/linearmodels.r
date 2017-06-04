e69e17f6d572437e212fbba1970cf4d50bc393f4
Ed Goodwin <ed.registration@gmail.com>
2017-06-04 15:55:23 -0500

    * separated bond model and ad model into separate files * cleaned out the comments to align with presentation * included broom in library list

--- R/linearmodels.r
+++ R/linearmodels.r
@@ -53,90 +53,16 @@ p_seg
 ## clear the plots
 dev.off()
 
-######### THE BOND DATASET ########
-bonds.dat = read.csv("http://www.stat.tamu.edu/~sheather/book/docs/datasets/bonds.txt", sep='\t')
-p = ggplot(data = bonds.dat, aes(CouponRate, BidPrice)) +
-  geom_point() +
-  ggtitle("BidPrice vs CouponRate")
-p
 
-bondmodel = lm(BidPrice ~ CouponRate, data = bonds.dat)
-
-pbond = p + geom_abline(aes(intercept = bondmodel$coefficients[1],
-                            slope = bondmodel$coefficients[2]),
-                        colour = "green") +
-  ggtitle("BidPrice vs CouponRate\n with Regression Line")
-pbond
-res = residuals(bondmodel)
-pre = predict(bondmodel)
-p_seg = pbond  + geom_segment(aes(x = CouponRate, y = BidPrice, xend = CouponRate, yend = pre), colour="red")
-p_seg
-
-## eliminate three outliers
-bondsclean.dat = bonds.dat[-c(4, 13, 35),]
-bondcleanmodel = lm(BidPrice ~ CouponRate, data = bondsclean.dat)
-p = ggplot(data = bondsclean.dat, aes(CouponRate, BidPrice)) +
-  geom_point() +
-  ggtitle("BidPrice vs CouponRate")
-p
-
-
-pbond = p + geom_abline(aes(intercept = bondcleanmodel$coefficients[1],
-                            slope = bondcleanmodel$coefficients[2]),
-                        colour = "green") +
-  ggtitle(" Vanilla Bond")
-pbond
-res = residuals(bondcleanmodel)
-pre = predict(bondcleanmodel)
-p_seg = pbond  + geom_segment(aes(x = CouponRate, y = BidPrice,
-                                  xend = CouponRate, yend = pre),
-                              colour="red")
-p_seg
-
-## clear the plots
-dev.off()
-
-## Advertising data set from ISLR
-ad.dat = read.csv("http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv")
-
-## Let's look at the ad data
-summary(ad.dat)
-pairs(ad.dat)
-corrgram(ad.dat)
-
-sales.mod = lm(data = ad.dat, Sales ~ TV + Newspaper + Radio)
-tidy(sales.mod)
-pairs(ad.dat)
-corrgram(ad.dat)
 
 ## F-test, assumptions (normality)
-
-
-
-
-
 
 # ## Presentation
 # lm(formula, data, subset, weights, na.action,
 #    method = "qr", model = TRUE, x = FALSE, y = FALSE, qr = TRUE,
 #    singular.ok = TRUE, contrasts = NULL, offset, ...)
-#
+
 ## what is a linear model?
 ## when can we use it? quantitative, normal data
 ## what is the lm formula and its inputs?
 ## what are the downsides to this model? relatively inflexible and simple
-# Given that estimation is undertaken on the basis of a least
-# squares analysis, estimates of the unknown parameters βj are determined by
-# minimising a sum of squares function {\displaystyle S=\sum
-# _{i=1}^{n}\left(Y_{i}-\beta _{0}-\beta _{1}\phi _{1}(X_{i1})-\cdots -\beta
-# _{p}\phi _{p}(X_{ip})\right)^{2}.} S=\sum _{{i=1}}^{n}\left(Y_{i}-\beta
-# _{0}-\beta _{1}\phi _{1}(X_{{i1}})-\cdots -\beta _{p}\phi
-# _{p}(X_{{ip}})\right)^{2}. From this, it can readily be seen that the "linear"
-# aspect of the model means the following: the function to be minimised is a
-# quadratic function of the βj for which minimisation is a relatively simple
-# problem; the derivatives of the function are linear functions of the βj making
-# it easy to find the minimising values; the minimising values βj are linear
-# functions of the observations Yi; the minimising values βj are linear
-# functions of the random errors εi which makes it relatively easy to determine
-# the statistical properties of the estimated values of βj.
-
